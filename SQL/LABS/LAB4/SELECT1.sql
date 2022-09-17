4fd6621c537212bd9ca85f11dc341947245a8041
dvynohradovv <dimarkens@gmail.com>
2022-09-17 20:28:57 +0300

    Finished SELECT1

--- SQL/LABS/LAB4/SELECT1.sql
+++ SQL/LABS/LAB4/SELECT1.sql
@@ -1,6 +1,7 @@
 -- 2.Получить полную детальную информацию об операторах, которые за указанный период приняли звонки с максимальной продолжительностью.
 SELECT
-    DISTINCT SUM(cl.duration) AS duration_sum,
+    MAX(cl.duration) AS max_duration,
+    op.id,
     op.isdisabled,
     op.first_name,
     op.last_name,
@@ -10,13 +11,13 @@ SELECT
     op.tenant_company_id
 FROM
     operator AS op
-    LEFT JOIN call_log AS cl ON cl.operator_id = op.id
+    JOIN call_log AS cl ON cl.operator_id = op.id
     JOIN tenant_company AS tc On tc.id = op.tenant_company_id
 WHERE
-    AND op.tenant_company_id = 1
-    AND cl.start_time >= '2000-12-12'
+    cl.start_time >= '2000-12-12'
     AND cl.start_time <= '2020-12-12'
 GROUP BY
+    op.id,
     op.isdisabled,
     op.first_name,
     op.last_name,
