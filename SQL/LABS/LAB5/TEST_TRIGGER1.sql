d77f09e4d10b2cca6f49930f6e2885006c6822bb
dvynohradovv <dimarkens@gmail.com>
2022-09-19 00:11:10 +0300

    FINISHED TRIGGER1

---
+++ SQL/LABS/LAB5/TEST_TRIGGER1.sql
@@ -0,0 +1,30 @@
+insert into
+    call_log (
+        id,
+        start_time,
+        duration,
+        caller_person_id,
+        caller_person_message,
+        tenant_company_phone_number_id,
+        operator_id,
+        operator_message,
+        disconnect_initiator,
+        response,
+        us_city_id,
+        additional_address
+    )
+values
+    (
+        2001,
+        '2013-04-30 00:28:29',
+        77,
+        590,
+        null,
+        1,
+        222,
+        null,
+        'Origination',
+        'Request Terminated',
+        null,
+        null
+    );
