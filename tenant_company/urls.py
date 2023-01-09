8019675aa4aec2adcf1f418d137545c1cc7cd41b
dvynohradovv <dimarkens@gmail.com>
2023-01-09 22:08:39 +0200

    Add call_logs, operators endpoints

--- tenant_company/urls.py
+++ tenant_company/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from tenant_company.views import CallLogView
+from tenant_company.views import CallLogListCreateView, OperatorListCreateView
 
 urlpatterns = [
     path('home', TemplateView.as_view(
         template_name="tenant_company/workflow/home.html"), name='tenant_company.home'),
-    path('call-logs', CallLogView.as_view(), name='tenant_company.call_logs'),
-    path('operators', TemplateView.as_view(
-        template_name="tenant_company/workflow/home.html"), name='tenant_company.operators'),
+    path('call-logs', CallLogListCreateView.as_view(),
+         name='tenant_company.call_logs'),
+    path('operators', OperatorListCreateView.as_view(),
+         name='tenant_company.operators'),
     path('work-places', TemplateView.as_view(
         template_name="tenant_company/workflow/home.html"), name='tenant_company.work_places'),
     path('phone-numbers', TemplateView.as_view(
--- tenant_company/views.py
+++ tenant_company/views.py
@@ -1,14 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
+from django.db.models import Q
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import View
 from django.views.generic import TemplateView
 
-from call_center_project.models import AccountType, CallLog
+from call_center_project.models import AccountType, CallLog, User
 
 
-class CallLogView(TemplateView):
+class CallLogListCreateView(TemplateView, LoginRequiredMixin):
     template_name = "tenant_company/workflow/call_logs.html"
 
     def get(self, request):
@@ -18,4 +16,19 @@ class CallLogView(TemplateView):
         )
 
         data = {'data': call_logs}
+        return render(request, self.template_name, data)
+
+
+class OperatorListCreateView(TemplateView, LoginRequiredMixin):
+    template_name = "tenant_company/workflow/operators.html"
+
+    def get(self, request):
+        user = request.user
+        operators = User.objects.filter(
+            Q(tenant_company__id=user.tenant_company.id)
+            &
+            Q(type=AccountType.Operator.value)
+        )
+
+        data = {'data': operators}
         return render(request, self.template_name, data)
